901934a26f0dd04545b3884f89397c4fa2f2edce
Marco Napetti <nappa85@therocktrading.com>
2022-12-02 09:16:15 +0100

    day2

---
+++ day2/src/main.rs
@@ -0,0 +1,135 @@
+const INPUT: &[u8] = include_bytes!("../input");
+const SUM_AS_MINE: u16 = get_sum_as_mine();
+const SUM_AS_OUTCOME: u16 = get_sum_as_outcome();
+
+const fn get_sum_as_mine() -> u16 {
+    let mut index = 0;
+    let mut sum = 0;
+    while index < INPUT.len() {
+        sum += Elf::new(INPUT[index]).add_mine(Mine::new(INPUT[index + 2])) as u16;
+        index += 4;
+    }
+    sum
+}
+
+const fn get_sum_as_outcome() -> u16 {
+    let mut index = 0;
+    let mut sum = 0;
+    while index < INPUT.len() {
+        sum += Elf::new(INPUT[index]).add_outcome(Outcome::new(INPUT[index + 2])) as u16;
+        index += 4;
+    }
+    sum
+}
+
+#[repr(u8)]
+#[derive(Debug, Copy, Clone)]
+enum Elf {
+    Rock = 1,
+    Paper = 2,
+    Scissor = 3,
+}
+
+impl Elf {
+    const fn new(b: u8) -> Self {
+        match b {
+            b'A' => Elf::Rock,
+            b'B' => Elf::Paper,
+            b'C' => Elf::Scissor,
+            _ => unreachable!(),
+        }
+    }
+
+    const fn add_mine(self, rhs: Mine) -> u8 {
+        rhs as u8
+            + match (self, rhs) {
+                (Elf::Rock, Mine::Rock)
+                | (Elf::Paper, Mine::Paper)
+                | (Elf::Scissor, Mine::Scissor) => 3, //draw
+                (Elf::Rock, Mine::Paper)
+                | (Elf::Paper, Mine::Scissor)
+                | (Elf::Scissor, Mine::Rock) => 6, //win
+                (Elf::Paper, Mine::Rock)
+                | (Elf::Scissor, Mine::Paper)
+                | (Elf::Rock, Mine::Scissor) => 0, //lose
+            }
+    }
+
+    const fn add_outcome(self, rhs: Outcome) -> u8 {
+        rhs as u8
+            + match (self, rhs) {
+                (Elf::Rock, Outcome::Draw)
+                | (Elf::Paper, Outcome::Lose)
+                | (Elf::Scissor, Outcome::Win) => Mine::Rock as u8,
+                (Elf::Rock, Outcome::Win)
+                | (Elf::Paper, Outcome::Draw)
+                | (Elf::Scissor, Outcome::Lose) => Mine::Paper as u8,
+                (Elf::Paper, Outcome::Win)
+                | (Elf::Scissor, Outcome::Draw)
+                | (Elf::Rock, Outcome::Lose) => Mine::Scissor as u8,
+            }
+    }
+}
+
+#[repr(u8)]
+#[derive(Debug, Copy, Clone)]
+enum Mine {
+    Rock = 1,
+    Paper = 2,
+    Scissor = 3,
+}
+
+impl Mine {
+    const fn new(b: u8) -> Self {
+        match b {
+            b'X' => Mine::Rock,
+            b'Y' => Mine::Paper,
+            b'Z' => Mine::Scissor,
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[repr(u8)]
+#[derive(Debug, Copy, Clone)]
+enum Outcome {
+    Lose = 0,
+    Draw = 3,
+    Win = 6,
+}
+
+impl Outcome {
+    const fn new(b: u8) -> Self {
+        match b {
+            b'X' => Outcome::Lose,
+            b'Y' => Outcome::Draw,
+            b'Z' => Outcome::Win,
+            _ => unreachable!(),
+        }
+    }
+}
+
+fn main() {
+    println!("Sum as mine: {SUM_AS_MINE}");
+    println!("Sum as outcome: {SUM_AS_OUTCOME}");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn first() {
+        let sum = include_bytes!("../input")
+            .chunks(4)
+            .map(|s| super::Elf::new(s[0]).add_mine(super::Mine::new(s[2])) as u16)
+            .sum::<u16>();
+        assert_eq!(sum, super::SUM_AS_MINE); //13009
+    }
+    #[test]
+    fn second() {
+        let sum = include_bytes!("../input")
+            .chunks(4)
+            .map(|s| super::Elf::new(s[0]).add_outcome(super::Outcome::new(s[2])) as u16)
+            .sum::<u16>();
+        assert_eq!(sum, super::SUM_AS_OUTCOME); //13009
+    }
+}
