4f46b2ec46c608554f5dad36b162206935cc3d9a
Marco Napetti <nappa85@therocktrading.com>
2022-12-06 10:02:03 +0100

    const eval

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,28 +1,94 @@
-fn main() {
-    let s = include_str!("../input");
-    let sum = s
-        .lines()
-        .map(|r| r.split_at(r.len() / 2))
-        .map(|(s1, s2)| s1.chars().find(|c| s2.contains(*c)).unwrap())
-        .map(to_priority)
-        .sum::<u32>();
-    println!("{sum}"); // 4016
-    let lines = s.lines().collect::<Vec<_>>();
-    let sum = lines
-        .chunks(3)
-        .flat_map(|s| {
-            s[0].chars()
-                .find(|c| s[1].contains(*c) && s[2].contains(*c))
-        })
-        .map(to_priority)
-        .sum::<u32>();
-    println!("{sum}"); // 4016
+const INPUT: &[u8] = include_bytes!("../input");
+const NUM_LINES: usize = get_num_lines();
+const LINES: [(usize, usize); NUM_LINES] = get_lines();
+const FIRST: u16 = first();
+const SECOND: u16 = second();
+
+const fn get_num_lines() -> usize {
+    let mut i = 0;
+    let mut o = 0;
+    while i < INPUT.len() {
+        if INPUT[i] == 10 {
+            o += 1;
+        }
+        i += 1;
+    }
+    o
 }
 
-fn to_priority(c: char) -> u32 {
-    match c as u32 {
-        i if i >= 97 => i - 96, // a
-        i if i >= 65 => i - 38, // A
+const fn get_lines() -> [(usize, usize); NUM_LINES] {
+    let mut i = 0;
+    let mut l = 0;
+    let mut o = [(0, 0); NUM_LINES];
+    while i < INPUT.len() {
+        if INPUT[i] == 10 {
+            o[l].1 = i;
+            l += 1;
+            if l < NUM_LINES {
+                o[l].0 = i + 1;
+            }
+        }
+        i += 1;
+    }
+    o
+}
+
+const fn first() -> u16 {
+    let mut o = 0;
+    let mut l = 0;
+    while l < NUM_LINES {
+        let middle = (LINES[l].0 + LINES[l].1) / 2;
+        let mut i = LINES[l].0;
+        'top: while i < middle {
+            let mut j = middle;
+            while j < LINES[l].1 {
+                if INPUT[i] == INPUT[j] {
+                    o += to_priority(INPUT[i]);
+                    break 'top;
+                }
+                j += 1;
+            }
+            i += 1;
+        }
+        l += 1;
+    }
+    o
+}
+
+const fn second() -> u16 {
+    let mut o = 0;
+    let mut l = 0;
+    while l < NUM_LINES {
+        let mut i = LINES[l].0;
+        'top: while i < LINES[l].1 {
+            let mut j = LINES[l + 1].0;
+            while j < LINES[l + 1].1 {
+                let mut z = LINES[l + 2].0;
+                while z < LINES[l + 2].1 {
+                    if INPUT[i] == INPUT[j] && INPUT[i] == INPUT[z] {
+                        o += to_priority(INPUT[i]);
+                        break 'top;
+                    }
+                    z += 1;
+                }
+                j += 1;
+            }
+            i += 1;
+        }
+        l += 3;
+    }
+    o
+}
+
+fn main() {
+    println!("first {FIRST}");
+    println!("second {SECOND}");
+}
+
+const fn to_priority(c: u8) -> u16 {
+    match c {
+        i if i >= 97 => i as u16 - 96, // a
+        i if i >= 65 => i as u16 - 38, // A
         _ => unreachable!(),
     }
 }
@@ -31,7 +97,34 @@ fn to_priority(c: char) -> u32 {
 mod tests {
     #[test]
     fn to_priority() {
-        assert_eq!(super::to_priority('a'), 1);
-        assert_eq!(super::to_priority('A'), 27);
+        assert_eq!(super::to_priority(b'a'), 1);
+        assert_eq!(super::to_priority(b'A'), 27);
+    }
+
+    #[test]
+    fn first() {
+        let s = include_str!("../input");
+        let sum = s
+            .lines()
+            .map(|r| r.split_at(r.len() / 2))
+            .map(|(s1, s2)| s1.chars().find(|c| s2.contains(*c)).unwrap())
+            .map(|c| super::to_priority(c as u8))
+            .sum::<u16>();
+        assert_eq!(sum, super::FIRST); // 8072
+    }
+
+    #[test]
+    fn second() {
+        let s = include_str!("../input");
+        let lines = s.lines().collect::<Vec<_>>();
+        let sum = lines
+            .chunks(3)
+            .flat_map(|s| {
+                s[0].chars()
+                    .find(|c| s[1].contains(*c) && s[2].contains(*c))
+            })
+            .map(|c| super::to_priority(c as u8))
+            .sum::<u16>();
+        assert_eq!(sum, super::SECOND); // 2567
     }
 }
